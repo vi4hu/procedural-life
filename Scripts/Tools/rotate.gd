5f981cb86852e94d2ff87909c86cbde31f04f6ff
rain-kun <rainkun3@protonmail.com>
2020-12-14 01:16:40 +0530

    better UI and gifs

--- Scripts/Tools/rotate.gd
+++ Scripts/Tools/rotate.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 onready var createworld = $"../CreateWorld"
 onready var cam = $Camera
-
+var rotate:= false
 
 func _ready():
 	pass
@@ -12,5 +12,6 @@ func _process(delta):
 	translation.x = createworld.map_size.x
 	translation.z = createworld.map_size.z
 	cam.translation.x = -max(translation.x ,translation.z)
-	rotation += Vector3(0,delta,0)
+	if rotate:
+		rotation += Vector3(0,delta,0)
 #
